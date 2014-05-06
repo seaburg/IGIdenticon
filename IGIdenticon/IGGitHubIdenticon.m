b2c0484fbd156e2d97b7b1deeee570e2efa81044
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2014-05-06 22:33:50 +0400

    Fix indents

--- IGIdenticon/IGGitHubIdenticon.m
+++ IGIdenticon/IGGitHubIdenticon.m
@@ -18,10 +18,10 @@
     }
 
     NSInteger blue = (number >> 16) & 31;
-	NSInteger green = (number >> 21) & 31;
-	NSInteger red = (number >> 27) & 31;
+    NSInteger green = (number >> 21) & 31;
+    NSInteger red = (number >> 27) & 31;
 
-	UIColor *foregroundColor = [UIColor colorWithRed:(red * 8) / 255.0f green:(green * 8) / 255.0f blue:(blue * 8) / 255.0f alpha:1];
+    UIColor *foregroundColor = [UIColor colorWithRed:(red * 8) / 255.0f green:(green * 8) / 255.0f blue:(blue * 8) / 255.0f alpha:1];
 
     UIGraphicsBeginImageContextWithOptions(CGSizeMake(size, size), NO, [UIScreen mainScreen].scale);
     CGContextRef context = UIGraphicsGetCurrentContext();
