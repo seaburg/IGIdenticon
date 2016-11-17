efce3b6f30be6caa454723989d220a8783977389
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-11-17 22:31:37 +0300

    Add  attribute to  method

--- IGIdenticon/IGImageGenerator.h
+++ IGIdenticon/IGImageGenerator.h
@@ -14,6 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface IGImageGenerator : NSObject
 
+- (instancetype)init NS_UNAVAILABLE;
+
 - (instancetype)initWithImageProducer:(id<IGImageProducing>)imageProducer hashFunction:(uint32_t(*)(NSData *))hashFunction NS_DESIGNATED_INITIALIZER;
 
 - (UIImage *)imageFromUInt32:(uint32_t)number size:(CGSize)size;
