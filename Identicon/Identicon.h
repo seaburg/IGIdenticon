91eac0b28f693bf49c87dbeb13fee4fbca3762b3
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-11-21 22:23:27 +0300

    Create project to build framawork

---
+++ Identicon/Identicon.h
@@ -0,0 +1,17 @@
+//
+//  Identicon.h
+//  Identicon
+//
+//  Created by Evgeniy Yurtaev on 19/11/2016.
+//  Copyright © 2016 Evgeniy Yurtaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Identicon.
+FOUNDATION_EXPORT double IdenticonVersionNumber;
+
+//! Project version string for Identicon.
+FOUNDATION_EXPORT const unsigned char IdenticonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Identicon/PublicHeader.h>
